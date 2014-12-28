df00c8292ef78eacade9bab528353afbaf069459
Water-Drop <water_drop@foxmail.com>
2014-12-28 11:06:44 +0800

    add JDBC helper

---
+++ src/util/JDBCHelper.java
@@ -0,0 +1,73 @@
+package util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class JDBCHelper {
+	private String url;
+	private String username;
+	private String password;
+
+	// Load MySql
+	public JDBCHelper() {
+		InputStream inputStream = this.getClass().getClassLoader()
+				.getResourceAsStream("dbConfig.properties");
+		Properties p = new Properties();
+		try {
+			p.load(inputStream);
+			url = p.getProperty("DatabaseConnectionAddress") + p.getProperty("DatabaseSchemaName");
+			username = p.getProperty("DatabaseConnectionUsername");
+			password = p.getProperty("DatabaseConnectionPassword");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("url:" + url + "uername:" + username + "password" + password);
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	// Get Connection
+	public Connection getConnection() {
+		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection(url, username, password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+
+	public void close(Connection conn, Statement st, ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		if (st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
