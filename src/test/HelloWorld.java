66f9b1a988e55511e3463f2495253c56422c8330
Water-Drop <water_drop@foxmail.com>
2014-12-28 11:06:33 +0800

    add test related

---
+++ src/test/HelloWorld.java
@@ -0,0 +1,18 @@
+package test;
+//It's just for testing!
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+	@Path("/HelloWorld")
+public class HelloWorld {
+	 
+	 @GET
+	 @Produces(MediaType.TEXT_PLAIN)
+	 public String helloWorld(){
+	  String ret = "Hello World!";
+	  return ret;
+	}
+}
+
---
+++ src/test/TestServlet.java
@@ -0,0 +1,61 @@
+package test;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import util.JDBCHelper;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet("/TestServlet")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		JDBCHelper jd = new JDBCHelper();
+		
+		try {
+			Connection conn = jd.getConnection();
+			PreparedStatement ps = conn
+					.prepareStatement("SELECT * FROM user");
+			ResultSet rs = ps.executeQuery();
+			rs.last();
+			int count = rs.getRow();
+			System.out.println("User count: " + count);
+		}catch (SQLException e) {
+			System.out.println(e.getMessage());
+		} catch (Exception e){
+			System.out.println(e.getMessage());
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
