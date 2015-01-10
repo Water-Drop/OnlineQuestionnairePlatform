c397d2a8db90e36974a4968c3968f0ce64cbf255
Water-Drop <water_drop@foxmail.com>
2015-01-10 11:06:43 +0800

    modify QN design

--- src/biz/QNDesign.java
+++ src/biz/QNDesign.java
@@ -1,7 +1,10 @@
 package biz;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.ws.rs.Path;
 
 import model.Question;
 import model.Questionnaire;
@@ -11,23 +14,31 @@ import dao.QuestionnaireDAO;
 import dao.impl.QuestionDAOimpl;
 import dao.impl.QuestionnaireDAOimpl;
 
+@Path("/design")
 public class QNDesign {
 	QuestionDAO qd = new QuestionDAOimpl();
 	QuestionnaireDAO qnd = new QuestionnaireDAOimpl();
-	public String addQuestionnaire(Integer uid, String param){
+	public String addQuestionnaire(String param){
+		Integer status = -1;
+		Integer qnid = -1;
 		JSONObject paramjson = JSONObject.fromObject(param);
 		Questionnaire qn = new Questionnaire();
-		qn.setUid(uid);
+		qn.setUid(Integer.parseInt(paramjson.getString("uid")));
 		qn.setTitle(paramjson.getString("title"));
 		qn.setDescription(paramjson.getString("description"));
+		qn.setCreateTime(new Date());
 		qn.setType(paramjson.getInt("type"));
-		Integer rtn = qnd.addQuestionnaire(qn);
+		qnid = qnd.addQuestionnaire(qn);
+		if (qnid > 0){
+			status = 0;
+		}
 		Map<String, String> map = new HashMap<String, String>();
-		map.put("status", rtn.toString());
+		map.put("status", status.toString());
+		map.put("qnid", qnid.toString());
 		JSONObject json = JSONObject.fromObject(map);
 		return json.toString();
 	}
-	public String addQuestion(Integer qnid, String param){
+	public String addQuestion(String param){
 		JSONObject paramjson = JSONObject.fromObject(param);
 		Question q = new Question();
 		q.setQnid(qnid);
